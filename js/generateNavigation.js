0a11d83f6f9b7fb2f9ae2f58ab34bf7c919c5700
Michael B <michael@ramyun.co.uk>
2023-07-19 20:13:13 +0100

    Update for 2023
    - update bio
    - add LinkedIn
    - pulled navigation out to JS
    - add Sloth Search project

---
+++ js/generateNavigation.js
@@ -0,0 +1,17 @@
+const PATHS = [
+  { name: "About", url: "/" },
+  { name: "Projects", url: "/projects" },
+  { name: "Publications", url: "/publications" },
+];
+
+const getCurrentPath = () =>
+  `/${window.location.pathname.replace(/\/$/, "").split("/").pop()}`;
+
+document.write(
+  `<nav>${PATHS.map(
+    (page) =>
+      `<a href="${page.url}" ${
+        getCurrentPath() === page.url ? `class="selected"` : ""
+      }>${page.name}</a>`
+  ).join("")}</nav>`
+);
